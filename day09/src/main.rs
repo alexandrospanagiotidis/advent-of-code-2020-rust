94a4c25852f053e00da2ae6099f23f3174e6094c
Alexandros Panagiotidis <alexandros@panagiotidis.eu>
2020-12-09 20:55:40 +0100

    Add day 9 part 2
    https://adventofcode.com/2020/day/9#part2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{BufRead, stdin};
-use std::ops::Add;
+use std::ops::{Add, Range};
 
 type NumberType = u64;
 
@@ -14,38 +14,84 @@ fn main() {
         )
         .collect();
 
-    part1(&numbers);
+    if let Some(part1_result) = part1(&numbers) {
+        let magic_number = part1_result.0;
+
+        println!("Did not find a number pair that sums to {0}", magic_number);
+
+        let windows = part2(&numbers, magic_number);
+
+        for window in windows {
+            let slice = &numbers[window.start..window.end];
+            let min_value = slice.iter().min().unwrap();
+            let max_value = slice.iter().max().unwrap();
+
+            println!("part2: {0:?} sums to {1}; sum(min={2}, max={3})={4}",
+                     slice, magic_number, min_value, max_value, min_value + max_value
+            );
+        }
+    }
 }
 
-fn part1(numbers: &[NumberType]) {
-    process_all_numbers(&numbers, 25, &sum_any_two);
+fn part1(numbers: &[NumberType]) -> Option<(NumberType, usize, usize)> {
+    process_all_numbers(&numbers, 25, &sum_any_two)
 }
 
-fn process_all_numbers(numbers: &[u64], window_size: usize, condition: &dyn Fn(&[NumberType], NumberType) -> Vec<NumberType>) {
+fn part2(numbers: &[NumberType], magic_number: NumberType) -> Vec<Range<usize>> {
+    let mut windows: Vec<Range<usize>> = Vec::new();
+
+    let length = numbers.len();
+
+    'search: for window_size in (2..=length / 2).rev() {
+        println!("window_size={0}", window_size);
+        for left in 0..length - window_size {
+            let right = left + window_size;
+
+            if right >= length {
+                continue 'search;
+            }
+
+            let slice = &numbers[left..right];
+            let sum: NumberType = slice.iter().sum();
+
+            // println!("sum={0} numbers[{1}..{2}]={3:?}", sum, left, right, slice);
+
+            if sum == magic_number {
+                windows.push(left..right);
+                // break 'search;
+            }
+        }
+    }
+
+    windows
+}
+
+fn process_all_numbers(numbers: &[u64], window_size: usize, condition: &dyn Fn(&[NumberType], NumberType) -> Vec<NumberType>) -> Option<(NumberType, usize, usize)> {
     let mut left = 0;
     let mut right = window_size;
 
-    loop {
+    while right <= numbers.len() {
         let next_index = right + 1;
 
         if next_index >= numbers.len() {
             panic!("No more numbers at index {0}", next_index);
         }
 
-        // +1 because upper bound is not inclusive
-        let candidates = &numbers[left..right + 1];
+        let candidates = &numbers[left..=right];
         let next_number = numbers[next_index];
         let operands = condition(candidates, next_number);
 
         if operands.is_empty() {
-            println!("Did not find a match for {0}", next_number);
-            println!("numbers[{0}..{1}]={2:?}", left, right, candidates);
-            break;
+            // println!("numbers[{0}..{1}]={2:?}", left, right, candidates);
+
+            return Some((next_number, left, right));
         }
 
         left += 1;
         right = left + window_size;
     }
+
+    None
 }
 
 fn sum_any_two(numbers: &[NumberType], needle: NumberType) -> Vec<NumberType> {
